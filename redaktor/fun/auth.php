b2e64f51fbd63b5fa861137ffe140bccff5c86aa
wja36rus <wja36rus@yandex.ru>
2022-11-01 14:52:27 +0300

    Update auth.php

--- redaktor/fun/auth.php
+++ redaktor/fun/auth.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 
-//$key = '80158fc467eedfff03d6c1514fe0ea48';
-$key = '1145a30ff80745b56fb0cecf65305017';
+$key = '80158fc467eedfff03d6c1514fe0ea48';
 
 $p = $_POST["password"];
 
